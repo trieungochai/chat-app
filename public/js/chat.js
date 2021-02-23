1cc29926fe5359c1101b68668b5a9b57ee15de31
Macbook <trieungochai.dev@gmail.com>
2021-02-23 19:04:16 +0700

    Socket.io Events (Send a welcome message to new users)

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,19 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-  console.log('The count has been updated!', count);
-});
+// server (emit) -> client (receive) - countUpdated
+// client (emit) -> server (receive) - increment
 
-document.querySelector('#increment').addEventListener('click', () => {
-  console.log('Clicked');
-  socket.emit('increment');
+// socket.on('countUpdated', (count) => {
+//   console.log('The count has been updated!', count);
+// });
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//   console.log('Clicked');
+//   socket.emit('increment');
+// });
+
+// Goal II: Send a welcome message to new users
+// 2. Have client listen for "message" event and print to console
+socket.on('message', (message) => {
+  console.log(message);
 });
--- src/index.js
+++ src/index.js
@@ -12,23 +12,34 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 // server (emit) -> client (receive) - countUpdated
 // client (emit) -> server (receive) - increment
+
+// let count = 0;
+
+// io.on('connection', (socket) => {
+//   console.log('New WebSocket connection!');
+
+//   socket.emit('countUpdated', count);
+
+//   socket.on('increment', () => {
+//     count++;
+//     // to emit the event to every connection available
+//     // socket.emit('countUpdated', count);
+//     // to emit the event to every single connection that's currently available
+//     io.emit('countUpdated', count);
+//   });
+// });
+
+// Goal II: Send a welcome message to new users
+// 1. Have server emit "message" when new client connects
+//    - Send "Welcome!" as the event data
+// 2. Have client listen for "message" event and print to console
 
 io.on('connection', (socket) => {
   console.log('New WebSocket connection!');
 
-  socket.emit('countUpdated', count);
-
-  socket.on('increment', () => {
-    count++;
-    // to emit the event to every connection available
-    // socket.emit('countUpdated', count);
-    // to emit the event to every single connection that's currently available
-    io.emit('countUpdated', count);
-  });
+  socket.emit('message', 'Welcome!');
 });
 
 server.listen(port, () => {
